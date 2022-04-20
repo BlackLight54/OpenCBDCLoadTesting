069fc35755614ecc379819ec705dc0b2e3b5122c
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-04-21 00:17:07 +0200

    Fixed CRLF

--- system_setup.sh
+++ system_setup.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 # ls && exit 0;
 mkdir -p $res_folder &&
 rm -f $res_folder/*  &&
-./system_launch.sh -r &&
+./system_launch.sh -r -b &&
 docker exec opencbdc-tx_client ./load_testing/gen_test_env.sh -w $wallet_csv -t $transaction_csv -n $numOfTxs -c $numOfWallets &&
 . ./test_launch.sh
 
