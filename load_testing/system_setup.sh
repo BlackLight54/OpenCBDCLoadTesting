cc4ca2a66db19fe51adbebe595a395f4d670688d
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-04-20 21:26:20 +0200

    Ready for JNI implementation

--- load_testing/system_setup.sh
+++ load_testing/system_setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-numOfTxs=500
-numOfWallets=50
-threadCount=5
-res_folder="./res"
-wallet_csv=${wallet_csv:-./load_testing/$res_folder/wallets.csv}
-transaction_csv=${transaction_csv:-./load_testing/$res_folder/transactions.csv}
-customSampler=true
+. ./test.cfg
+
+
+
 
 cd "$(dirname "$0")"
 # ls && exit 0;
@@ -14,5 +11,5 @@ mkdir -p $res_folder &&
 rm -f $res_folder/*  &&
 ./system_launch.sh -r &&
 docker exec opencbdc-tx_client ./load_testing/gen_test_env.sh -w $wallet_csv -t $transaction_csv -n $numOfTxs -c $numOfWallets &&
-./test_launch.sh
+. ./test_launch.sh
 
--- load_testing/test_launch.sh
+++ load_testing/test_launch.sh
@@ -1,12 +1,6 @@
 #!/bin/bash 
+. ./test.cfg
 
-threadCount=${threadCount:-5}
-numOfWallets=${numOfWallets-50}
-numOfTxs=${numOfTxs:-500}
-res_folder=${res_folder:-"./res"}
-wallet_csv=${wallet_csv:-./load_testing/$res_folder/wallets.csv}
-transaction_csv=${transaction_csv:-./load_testing/$res_folder/transactions.csv}
-customSampler=${customSampler:-true}
 
 
 if [ $customSampler == false ]
@@ -18,8 +12,10 @@ else
     echo "Compiling sampler" 
     cd ..
     cd JmeterCustomSampler
-    mvn 
-     echo "Starting Jmeter test with $threadCount threads on $numOfWallets wallets and $numOfTxs txs with Java JNI Sampler" &&
-    docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote_w_CustomSampler.jmx -l ./load_testing/$res_folder/results.jtl -j ./load_testing/$res_folder/jmeter.log -n -DtxCount=$numOfTxs -Dthreadcount=$threadCount 
+    mvn package &&
+    echo "copying sampler jar" &&
+    docker cp target/JmeterCustomSampler-1.0-SNAPSHOT.jar opencbdc-tx_client:/opt/apache-jmeter-5.4.3/lib/ext &&
+    echo "Starting Jmeter test with $threadCount threads on $numOfWallets wallets and $numOfTxs txs with Java JNI Sampler" &&
+    docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote.jmx -l ./load_testing/$res_folder/results.jtl -j ./load_testing/$res_folder/jmeter.log -n -JtxCount=$numOfTxs -JcustomSampler="true" -Dthreadcount=$threadCount 
     
 fi 
