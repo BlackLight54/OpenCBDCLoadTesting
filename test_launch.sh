7e6992d3382a5640257b97868d51250aeb445e62
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-04-21 11:44:39 +0200

    Removed Sampler Compilaton(moved to dockerfile)

--- test_launch.sh
+++ test_launch.sh
@@ -9,12 +9,12 @@ then
     docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote.jmx -l ./load_testing/$res_folder/results.jtl -j ./load_testing/$res_folder/jmeter.log -n -DtxCount=$numOfTxs -Dthreadcount=$threadCount 
     
 else 
-    echo "Compiling sampler" 
-    cd ..
-    cd JmeterCustomSampler
-    mvn package &&
-    echo "copying sampler jar" &&
-    docker cp target/JmeterCustomSampler-1.0-SNAPSHOT.jar opencbdc-tx_client:/opt/apache-jmeter-5.4.3/lib/ext &&
+    # echo "Compiling sampler" 
+    # cd ..
+    # cd JmeterCustomSampler
+    # mvn package &&
+    # echo "copying sampler jar" &&
+    # docker cp target/JmeterCustomSampler-1.0-SNAPSHOT.jar opencbdc-tx_client:/opt/apache-jmeter-5.4.3/lib/ext &&
     echo "Starting Jmeter test with $threadCount threads on $numOfWallets wallets and $numOfTxs txs with Java JNI Sampler" &&
     docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote.jmx -l ./load_testing/$res_folder/results.jtl -j ./load_testing/$res_folder/jmeter.log -n -JtxCount=$numOfTxs -JcustomSampler="true" -Dthreadcount=$threadCount 
     
